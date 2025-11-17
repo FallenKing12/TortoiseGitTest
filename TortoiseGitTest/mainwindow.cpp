bfa83aef3a6ba46ef950df36097428b4cf8433c2
FallenKing12 <arseny979@mail.ru>
2025-11-17 23:09:45 +0300

    Added 3D

--- TortoiseGitTest/mainwindow.cpp
+++ TortoiseGitTest/mainwindow.cpp
@@ -2,14 +2,74 @@
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
+    : QOpenGLWidget(parent)
     , ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::initializeGL()
+{
+
+}
+
+void MainWindow::resizeGL(int w, int h)
+{
+    glViewport(0, 0, w, h);
+}
+
+void MainWindow::paintGL()
+{
+    glClearColor(66./255, 170./255, 255./255, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    gluPerspective(45.0f, (float)100 / (float)100, 0.1f, 10000.0f);
+
+    gluLookAt(0, 100, 1, 0, 1, 0, 0, 1, 0);
+
+    int a = 10;
+    glBegin(GL_QUADS);
+
+        glColor3d(1,0,0);
+        glVertex3d(-a,0,a);
+        glVertex3d(-a,0,-a);
+        glVertex3d(-a,50,-a);
+        glVertex3d(-a,50,a);
+
+        glColor3d(0,1,0);
+        glVertex3d(-a,0,a);
+        glVertex3d(a,0,a);
+        glVertex3d(a,50,a);
+        glVertex3d(-a,50,a);
+
+        glColor3d(0,0,1);
+        glVertex3d(a,0,a);
+        glVertex3d(a,0,-a);
+        glVertex3d(a,50,-a);
+        glVertex3d(a,50,a);
+
+        glColor3d(1,1,0);
+        glVertex3d(a,0,-a);
+        glVertex3d(-a,0,-a);
+        glVertex3d(-a,50,-a);
+        glVertex3d(a,50,-a);
+
+        glColor3d(150./255,75./255,0);
+        glVertex3d(a,0,a);
+        glColor3d(1,75./255,0);
+        glVertex3d(a,0,-a);
+        glColor3d(150./255,1,0);
+        glVertex3d(-a,0,-a);
+        glColor3d(150./255,75./255,1);
+        glVertex3d(-a,0,a);
+
+        glEnd();
 }
 
--- TortoiseGitTest/mainwindow.h
+++ TortoiseGitTest/mainwindow.h
@@ -2,18 +2,26 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QOpenGLWidget>
+#include <QtOpenGL/QtOpenGL>
+#include <gl/GL.h>
+#include <gl/GLU.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
+class MainWindow : public QOpenGLWidget
 {
     Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+    void initializeGL();
+    void resizeGL(int w, int h);
+    void paintGL();
 
 private:
     Ui::MainWindow *ui;
